0c1fd0c0b8aba4d2d8f4897f1eef9d0e6c463e3f
andrewxu13 <72419513+andrewxu13@users.noreply.github.com>
2024-03-19 12:02:47 -0700

    Update streamlit_app.py

--- CHLA_Prediction/CHLA_Deployment/streamlit_app.py
+++ CHLA_Prediction/CHLA_Deployment/streamlit_app.py
@@ -1,7 +1,16 @@
 import logging
 
+
+
 # Configure logging
-logging.basicConfig(filename='./app.log', level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
+
+# Add file handler
+file_handler = logging.FileHandler('./app.log')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter('%(name)s - %(levelname)s - %(message)s'))
+logging.getLogger().addHandler(file_handler)
+
 
 
 import streamlit as st
@@ -18,49 +27,10 @@ date_string = datetime.datetime(2000, 1, 1)
 
 
 
-def main():
-    st.set_page_config(page_title="CHLA Prediction")
-    st.title("Patient Showup Prediction: ")
+import pandas as pd
+import streamlit as st
 
-    appt_date = st.date_input(label = "Appointment Date", value = today, min_value= date_string)
-    book_date = st.date_input(label = "Booking Date", value = today, min_value= date_string)
-    lead_time = st.text_input("Lead time", 0)
-    total_no_show = st.text_input("How many times this patient hasn't show up in the past?",0)
-    total_success = st.text_input("How many times this patient has show up for their appointment?",0)
-    total_cancel = st.text_input("How many times this patient has cancel for their appointment?",0)
-    total_reschedule = st.text_input("How many times this patient has reschedule for their appointment?",0)
-
-    if st.button("Predict"):
-        data = {
-        'appt_date': appt_date,
-        'book_date': book_date,
-        'lead_time': int(lead_time),
-        'total_no_show': int(total_no_show),
-        'total_success': int(total_success),
-        'total_cancel': int(total_cancel),
-        'total_reschedule': int(total_reschedule)}
-        
-    
-
-        predict_df = pd.DataFrame([data])
-
-        predict_df['appt_date'] = pd.to_datetime(predict_df['appt_date'])
-        predict_df['book_date'] = pd.to_datetime(predict_df['book_date'])
-
-        predict_df['NUM_OF_MONTH'] = predict_df['appt_date'].dt.month
-
-        # Now, all your features should be numerical, and you can attempt prediction
-        features_list = predict_df.drop(['appt_date','book_date' ],axis = 1).values
-        prediction = model.predict(features_list)
-        
-        if prediction == 1:
-            text = "Patient will not show up"
-        else:
-            text = "Patient will show up"
-
-        st.success(text)
-        
-# Refactor to a function for unit testing
+# Function to make prediction
 def make_prediction(data):
     predict_df = pd.DataFrame([data])
     predict_df['appt_date'] = pd.to_datetime(predict_df['appt_date'])
@@ -70,6 +40,39 @@ def make_prediction(data):
     prediction = model.predict(features_list)
     return prediction
 
+# Main function for Streamlit app
+def main():
+    st.set_page_config(page_title="Appointment Showup Prediction")
+    st.title("Appointment Showup Prediction")
 
-if __name__=='__main__':
+    appt_date = st.date_input(label="Appointment Date", value=today, min_value=date_string)
+    book_date = st.date_input(label="Booking Date", value=today, min_value=date_string)
+    lead_time = st.text_input("Lead time (in days)", 0)
+    total_no_show = st.text_input("Total number of previous no-shows", 0)
+    total_success = st.text_input("Total number of previous successful appointments", 0)
+    total_cancel = st.text_input("Total number of previous cancellations", 0)
+    total_reschedule = st.text_input("Total number of previous rescheduled appointments", 0)
+
+    if st.button("Predict"):
+        data = {
+            'appt_date': appt_date,
+            'book_date': book_date,
+            'lead_time': int(lead_time),
+            'total_no_show': int(total_no_show),
+            'total_success': int(total_success),
+            'total_cancel': int(total_cancel),
+            'total_reschedule': int(total_reschedule)
+        }
+
+        prediction = make_prediction(data)
+        
+        if prediction == 1:
+            text = "Prediction: Patient is likely to miss the appointment."
+        else:
+            text = "Prediction: Patient is likely to attend the appointment."
+
+        st.success(text)
+
+# Entry point for running the Streamlit app
+if __name__ == '__main__':
     main()
