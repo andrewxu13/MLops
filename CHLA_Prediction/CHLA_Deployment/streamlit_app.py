5e0718a6bedf3bac513daf6b8f3cf0c203dcf176
andrewxu13 <72419513+andrewxu13@users.noreply.github.com>
2024-04-23 01:01:54 -0700

    Update streamlit_app.py

--- CHLA_Prediction/CHLA_Deployment/streamlit_app.py
+++ CHLA_Prediction/CHLA_Deployment/streamlit_app.py
@@ -13,8 +13,7 @@ encoder_dict = pickle.load(open('./CHLA_Prediction/CHLA_Deployment/encoder_V2.pk
 # Load the dataset
 data_path = './CHLA_Prediction/CHLA_Deployment/CHLA_clean_data_2024_Appointments.csv'
 dataset = pd.read_csv(data_path)
-dataset.columns = dataset.columns.str.lower()
-dataset['appt_date'] = pd.to_datetime(dataset['appt_date'], errors='coerce')
+dataset['APPT_DATE'] = pd.to_datetime(dataset['APPT_DATE'], errors='coerce')
 
 # Function to process features, including dates
 def process_features(data, encoder_dict, date_columns=None):
@@ -42,27 +41,27 @@ def main():
     st.title("Appointment Showup Prediction")
 
     # User inputs for clinic and range of appointment dates
-    clinics = dataset['clinic'].dropna().unique()
+    clinics = dataset['CLINIC'].dropna().unique()
     clinic = st.selectbox("Select a Clinic", options=clinics)
-    appt_date_range = st.date_input("Select Appointment Date Range", value=[dataset['appt_date'].min(), dataset['appt_date'].max()], min_value=dataset['appt_date'].min(), max_value=dataset['appt_date'].max())
+    appt_date_range = st.date_input("Select Appointment Date Range", value=[dataset['APPT_DATE'].min(), dataset['APPT_DATE'].max()], min_value=dataset['APPT_DATE'].min(), max_value=dataset['APPT_DATE'].max())
 
     if st.button("Predict"):
         # Filter the dataset based on the selected clinic and appointment date range
-        filtered_data = dataset[(dataset['clinic'] == clinic) &
-        (dataset['appt_date'].dt.date >= appt_date_range[0]) &
-        (dataset['appt_date'].dt.date <= appt_date_range[1])]
+        filtered_data = dataset[(dataset['CLINIC'] == clinic) &
+        (dataset['APPT_DATE'].dt.date >= appt_date_range[0]) &
+        (dataset['APPT_DATE'].dt.date <= appt_date_range[1])]
 
         if not filtered_data.empty:
             # Process features for prediction, specifying which columns are dates
-            processed_data = process_features(filtered_data.copy(), encoder_dict, date_columns=['appt_date'])
+            processed_data = process_features(filtered_data.copy(), encoder_dict, date_columns=['APPT_DATE'])
             # Prepare features for prediction
-            predictions = model.predict(processed_data.drop(['is_noshow'], axis=1, errors='ignore'))
+            predictions = model.predict(processed_data.drop(['IS_NOSHOW'], axis=1, errors='ignore'))
 
             # Append the predictions to the filtered DataFrame
             filtered_data['Prediction'] = predictions
 
             if 'is_noshow' in filtered_data.columns:
-                accuracy = (filtered_data['Prediction'] == filtered_data['is_noshow']).mean() * 100
+                accuracy = (filtered_data['Prediction'] == filtered_data['IS_NOSHOW']).mean() * 100
                 st.success(f"Prediction Accuracy: {accuracy:.2f}%")
             
             # Display the complete DataFrame with predictions
