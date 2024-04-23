28c379af0a245e60f684dc7c00cf71a81075217b
andrewxu13 <72419513+andrewxu13@users.noreply.github.com>
2024-04-23 09:14:22 -0700

    Update streamlit_app.py

--- CHLA_Prediction/CHLA_Deployment/streamlit_app.py
+++ CHLA_Prediction/CHLA_Deployment/streamlit_app.py
@@ -6,35 +6,35 @@ import streamlit as st
 from sklearn import preprocessing
 
 # Load necessary resources
-MODEL = pickle.load(open('./CHLA_Prediction/CHLA_Deployment/random_forest.pkl', 'rb'))
-ENCODER_DICT = pickle.load(open('./CHLA_Prediction/CHLA_Deployment/encoder_V2.pkl', 'rb'))
-DATASET = pd.read_csv('./CHLA_Prediction/CHLA_Deployment/CHLA_clean_data_2024_Appointments.csv')
-DATASET.columns = DATASET.columns.str.upper()
-DATASET['APPT_DATE'] = pd.to_datetime(DATASET['APPT_DATE'], errors='coerce')
+model = pickle.load(open('./CHLA_Prediction/CHLA_Deployment/random_forest.pkl', 'rb'))
+encoder_dict = pickle.load(open('./CHLA_Prediction/CHLA_Deployment/encoder_V2.pkl', 'rb'))
+dataset = pd.read_csv('./CHLA_Prediction/CHLA_Deployment/CHLA_clean_data_2024_Appointments.csv')
+dataset.columns = dataset.columns.str.upper()
+dataset['APPT_DATE'] = pd.to_datetime(dataset['APPT_DATE'], errors='coerce')
 
 def main():
     st.title("Appointment Showup Prediction")
-    CLINICS = DATASET['CLINIC'].dropna().unique()
-    CLINIC = st.selectbox("Select a Clinic", options=CLINICS)
-    APPT_DATE_RANGE = st.date_input("Select Appointment Date Range", value=[DATASET['APPT_DATE'].min(), DATASET['APPT_DATE'].max()])
+    clinics = dataset['CLINIC'].dropna().unique()
+    clinic = st.selectbox("Select a Clinic", options=clinics)
+    appt_date_range = st.date_input("Select Appointment Date Range", value=[dataset['APPT_DATE'].min(), dataset['APPT_DATE'].max()])
 
     if st.button("Predict"):
-        FILTERED_DATA = DATASET[
-            (DATASET['CLINIC'] == CLINIC) &
-            (DATASET['APPT_DATE'].dt.date >= APPT_DATE_RANGE[0]) &
-            (DATASET['APPT_DATE'].dt.date <= APPT_DATE_RANGE[1])
+        filtered_data = dataset[
+            (dataset['CLINIC'] == clinic) &
+            (dataset['APPT_DATE'].dt.date >= appt_date_range[0]) &
+            (dataset['APPT_DATE'].dt.date <= appt_date_range[1])
         ]
-        FILTERED_DATA = FILTERED_DATA['LEAD_TIME','TOTAL_NUMBER_OF_NOSHOW', 'TOTAL_NUMBER_OF_SUCCESS_APPOINTMENT',
-        'TOTAL_NUMBER_OF_CANCELLATIONS', 'TOTAL_NUMBER_OF_RESCHEDULED', 'NUM_OF_MONTH']
+        filtered_data = filtered_data[['LEAD_TIME','TOTAL_NUMBER_OF_NOSHOW', 'TOTAL_NUMBER_OF_SUCCESS_APPOINTMENT',
+        'TOTAL_NUMBER_OF_CANCELLATIONS', 'TOTAL_NUMBER_OF_RESCHEDULED', 'NUM_OF_MONTH']]
         
-        if not FILTERED_DATA.empty:
-            PROCESSED_DATA = process_features(FILTERED_DATA.copy(), ENCODER_DICT)
-            PREDICTIONS = MODEL.predict(PROCESSED_DATA)
-            FILTERED_DATA['PREDICTION'] = PREDICTIONS[:len(FILTERED_DATA)]  # Ensure predictions align with filtered data
+        if not filtered_data.empty:
+            predictions = model.predict(filtered_data)
+            filtered_data['PREDICTION'] = predictions[:len(filtered_data)]  # Ensure predictions align with filtered data
 
-            st.write(FILTERED_DATA)
+            st.write(filtered_data)
         else:
             st.error("No data available for the selected clinic and date range.")
 
 if __name__ == '__main__':
     main()
+
