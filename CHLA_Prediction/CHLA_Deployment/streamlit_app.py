46b61ac36f8bdd2e3b8b3425ad00dfec9cb65f3b
andrewxu13 <72419513+andrewxu13@users.noreply.github.com>
2024-08-05 18:56:20 -0700

    Update streamlit_app.py

--- CHLA_Prediction/CHLA_Deployment/streamlit_app.py
+++ CHLA_Prediction/CHLA_Deployment/streamlit_app.py
@@ -19,24 +19,30 @@ def main():
     appt_date_range = st.date_input("Select Appointment Date Range", value=[dataset['APPT_DATE'].min(), dataset['APPT_DATE'].max()])
 
     if st.button("Predict"):
-        filtered_data = dataset[
+        # Filter data for prediction
+        prediction_data = dataset[
             (dataset['CLINIC'] == clinic) &
             (dataset['APPT_DATE'].dt.date >= appt_date_range[0]) &
             (dataset['APPT_DATE'].dt.date <= appt_date_range[1])
         ]
 
-        processed_data = filtered_data[['LEAD_TIME','TOTAL_NUMBER_OF_NOSHOW', 'TOTAL_NUMBER_OF_SUCCESS_APPOINTMENT',
-                                        'TOTAL_NUMBER_OF_CANCELLATIONS', 'TOTAL_NUMBER_OF_RESCHEDULED', 'NUM_OF_MONTH']]
+        # Prepare data for prediction (make sure to use the correct column names as per your model's requirements)
+        processed_data = prediction_data[['LEAD_TIME', 'TOTAL_NUMBER_OF_NOSHOW', 'TOTAL_NUMBER_OF_SUCCESS_APPOINTMENT',
+                                          'TOTAL_NUMBER_OF_CANCELLATIONS', 'TOTAL_NUMBER_OF_RESCHEDULED', 'NUM_OF_MONTH']]
 
-        
-        if not filtered_data.empty:
-            predictions = model.predict(processed_data)
-            filtered_data['PREDICTION'] = predictions[:len(processed_data)]  # Ensure predictions align with filtered data
+        if not processed_data.empty:
+            probabilities = model.predict_proba(processed_data)
+            predictions = np.argmax(probabilities, axis=1)  # Getting the class with the highest probability
+            prediction_data['PREDICTION'] = predictions
+            prediction_data['PROBABILITY'] = probabilities[:, 1]  # Assuming class 1 is the positive class
 
-            st.write(filtered_data)
+            # Filter columns for display after predictions
+            display_data = prediction_data[['MRN', 'APPT_DATE', 'BOOK_DATE', 'CLINIC', 'IS_NOSHOW', 'PREDICTION']]
+            display_data['PROBABILITY_BAR'] = prediction_data['PROBABILITY'].apply(lambda x: st.progress(x))
+
+            st.write(display_data)
         else:
             st.error("No data available for the selected clinic and date range.")
 
 if __name__ == '__main__':
     main()
-
